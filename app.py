1a64bf17da0cb3c490c7aeec1a2862cbc116dea0
Irakli Korpashvili <ikorpashvili@europebet.com>
2019-12-05 02:26:30 +0400

    Fixed not importing db when running the app

--- app.py
+++ app.py
@@ -13,12 +13,6 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'dj5hrb5729y3cypw6hqkwwezar6x8c6x'
 api = Api(app)
 
-
-@app.before_first_request
-def create_tables():
-    db.create_all()
-
-
 jwt = JWT(app, authenticate, identity)
 
 api.add_resource(Store, '/store/<string:name>')
